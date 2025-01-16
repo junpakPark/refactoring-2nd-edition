caaf05b14884ef82f50d29f43184548dec68afe9
junpakPark <112045553+junpakPark@users.noreply.github.com>
2025-01-16 14:03:55 +0900

    refactor: 총합을 위한 메서드 이동

--- src/main/java/me/junpak/refactoring/chapter1/after/StatementData.java
+++ src/main/java/me/junpak/refactoring/chapter1/after/StatementData.java
@@ -7,7 +7,9 @@ import me.junpak.refactoring.chapter1.data.Play;
 
 public record StatementData(
         String customer,
-        List<EnrichPerformance> performances
+        List<EnrichPerformance> performances,
+        int totalAmount,
+        int totalVolumeCredits
 ) {
 
     public static StatementData createStatementData(final Invoice invoice, final Map<String, Play> plays) {
@@ -15,6 +17,30 @@ public record StatementData(
                 .map(performance -> EnrichPerformance.of(performance, plays))
                 .toList();
 
-        return new StatementData(invoice.customer(), performances);
+        return new StatementData(
+                invoice.customer(),
+                performances,
+                totalAmount(performances),
+                totalVolumeCredits(performances)
+        );
     }
+
+
+    private static int totalAmount(final List<EnrichPerformance> performances) {
+        var result = 0;
+        for (var perf : performances) {
+            result += perf.amount();
+        }
+        return result;
+    }
+
+    private static int totalVolumeCredits(final List<EnrichPerformance> performances) {
+        var result = 0;
+        for (var perf : performances) {
+            result += perf.volumeCredits();
+        }
+        return result;
+    }
+
+
 }
