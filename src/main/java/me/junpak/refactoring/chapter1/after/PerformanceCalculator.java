348150b0c9b5b8f9c8aa030c4b181738e2099a1f
junpakPark <112045553+junpakPark@users.noreply.github.com>
2025-01-16 16:54:21 +0900

    refactor: volumeCreditsFor 메서드 이동

--- src/main/java/me/junpak/refactoring/chapter1/after/PerformanceCalculator.java
+++ src/main/java/me/junpak/refactoring/chapter1/after/PerformanceCalculator.java
@@ -35,6 +35,16 @@ public class PerformanceCalculator {
         return result;
     }
 
+    public int volumeCredits() {
+        var result = 0;
+        result += Math.max(this.performance.audience() - 30, 0);
+
+        if ("comedy".equals(this.play.type())) {
+            result += Math.floor(this.performance.audience() / 5);
+        }
+        return result;
+    }
+
     public Performance performance() {
         return performance;
     }
