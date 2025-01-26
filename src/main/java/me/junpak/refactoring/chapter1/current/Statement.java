6c1a5e1068de7c9f42476b439400836f0c6b9c63
junpakPark <112045553+junpakPark@users.noreply.github.com>
2025-01-26 21:06:43 +0900

    feat: htmlStatement 메서드 구현

--- src/main/java/me/junpak/refactoring/chapter1/current/Statement.java
+++ src/main/java/me/junpak/refactoring/chapter1/current/Statement.java
@@ -23,16 +23,39 @@ public class Statement {
         return renderPlainText(factory.createStatementData(invoice, plays));
     }
 
+    public String htmlStatement(Invoice invoice, Map<String, Play> plays) {
+        return renderHtml(factory.createStatementData(invoice, plays));
+    }
+
     private String renderPlainText(final StatementData data) {
         var result = new StringBuilder("청구 내역 (고객명: " + data.customer() + ")").append(LF);
 
         for (var perf : data.performances()) {
-            result.append(String.format("  %s: %s원 (%d석)", perf.play().name(), usd(perf.amount()),
-                    perf.audience())).append(LF);
+            result.append(String.format(
+                    "  %s: %s원 (%d석)",
+                    perf.play().name(),
+                    usd(perf.amount()),
+                    perf.audience()
+            )).append(LF);
         }
 
         result.append(String.format("총액: %s원", usd(data.totalAmount()))).append(LF);
         result.append(String.format("적립 포인트: %d점", data.totalVolumeCredits())).append(LF);
+
+        return result.toString();
+    }
+
+    private String renderHtml(final StatementData data) {
+        var result = new StringBuilder(String.format("<h1>청구 내역 (고객명: %s)</h1>", data.customer())).append(LF);
+        result.append("<table>").append(LF);
+        result.append("  <tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>").append(LF);
+        for (var perf : data.performances()) {
+            result.append(String.format("  <tr><td>%s</td><td>%s</td>", perf.play().name(), perf.audience()));
+            result.append(String.format("<td>%s</td></tr>", usd(perf.amount()))).append(LF);
+        }
+        result.append("</table>").append(LF);
+        result.append(String.format("<p>총액: <em>%s</em></p>", usd(data.totalAmount()))).append(LF);
+        result.append(String.format("<p>적립 포인트: <em>%s</em>점</p>", data.totalVolumeCredits())).append(LF);
 
         return result.toString();
     }
--- src/test/java/me/junpak/refactoring/chapter1/current/StatementTest.java
+++ src/test/java/me/junpak/refactoring/chapter1/current/StatementTest.java
@@ -14,6 +14,8 @@ import org.junit.jupiter.api.Test;
 
 class StatementTest {
 
+    private final Statement sut = new Statement(new StatementFactory());
+
     private Map<String, Play> plays;
     private List<Invoice> invoices;
 
@@ -41,7 +43,6 @@ class StatementTest {
     @Test
     void statement() {
         // given
-        final Statement statement = new Statement(new StatementFactory());
         final Invoice invoice = invoices.get(0);
         final String expected = """
                 청구 내역 (고객명: BigCo)
@@ -53,10 +54,32 @@ class StatementTest {
                 """;
 
         // when
-        final String result = statement.statement(invoice, plays);
+        final String result = sut.statement(invoice, plays);
 
         // then
         assertThat(result).isEqualTo(expected);
     }
+
+    @Test
+    void htmlStatement() {
+        // given
+        final Invoice invoice = invoices.get(0);
+        final String expected = """
+                <h1>청구 내역 (고객명: BigCo)</h1>
+                <table>
+                  <tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>
+                  <tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>
+                  <tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>
+                  <tr><td>Othello</td><td>40</td><td>$500.00</td></tr>
+                </table>
+                <p>총액: <em>$1,730.00</em></p>
+                <p>적립 포인트: <em>47</em>점</p>
+                """;
+        // when
+        final String result = sut.htmlStatement(invoice, plays);
+        // then
+        assertThat(result).isEqualTo(expected);
+    }
+
 
 }
