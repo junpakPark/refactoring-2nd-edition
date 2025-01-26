df25d6c3ab8da5161437c4045aa512d8b160928d
junpakPark <112045553+junpakPark@users.noreply.github.com>
2025-01-26 19:50:16 +0900

    refactor: format 메서드 선언 변경

--- src/main/java/me/junpak/refactoring/chapter1/current/Statement.java
+++ src/main/java/me/junpak/refactoring/chapter1/current/Statement.java
@@ -27,20 +27,20 @@ public class Statement {
             result.append(String.format(
                             "  %s: %s원 (%d석)",
                             plays.get(perf.playID()).name(),
-                            format(amountFor(perf, plays.get(perf.playID())) / 100.0),
+                            usd(amountFor(perf, plays.get(perf.playID())) / 100.0),
                             perf.audience()
                     ))
                     .append(LF);
             totalAmount += amountFor(perf, plays.get(perf.playID()));
         }
 
-        result.append(String.format("총액: %s원", format(totalAmount / 100.0))).append(LF);
+        result.append(String.format("총액: %s원", usd(totalAmount / 100.0))).append(LF);
         result.append(String.format("적립 포인트: %d점", volumeCredits)).append(LF);
 
         return result.toString();
     }
 
-    private String format(double amount) {
+    private String usd(double amount) {
         return US_CURRENCY_FORMAT.format(amount);
     }
 
