f09557c05b51e6883217dc227e4cf25f01680a74
junpakPark <112045553+junpakPark@users.noreply.github.com>
2025-01-26 22:16:39 +0900

    style: 누락된 final 추가

--- src/main/java/me/junpak/refactoring/chapter1/current/controller/StatementController.java
+++ src/main/java/me/junpak/refactoring/chapter1/current/controller/StatementController.java
@@ -6,7 +6,6 @@ import me.junpak.refactoring.chapter1.current.model.data.Invoice;
 import me.junpak.refactoring.chapter1.current.model.data.Play;
 import me.junpak.refactoring.chapter1.current.view.OutputView;
 
-
 public class StatementController {
 
     private final StatementFactory factory;
@@ -17,11 +16,11 @@ public class StatementController {
         this.view = outputView;
     }
 
-    public String statement(Invoice invoice, Map<String, Play> plays) {
+    public String statement(final Invoice invoice, final Map<String, Play> plays) {
         return view.renderPlainText(factory.createStatement(invoice, plays));
     }
 
-    public String htmlStatement(Invoice invoice, Map<String, Play> plays) {
+    public String htmlStatement(final Invoice invoice, final Map<String, Play> plays) {
         return view.renderHtml(factory.createStatement(invoice, plays));
     }
 
